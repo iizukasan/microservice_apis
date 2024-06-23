9daa3ab5c90dc109dddd01fe25186576e5c7c924
iizk <iizkmaiko@gmail.com>
2024-06-23 13:45:43 +0900

    add: response model for view functions

--- ch02/orders/api/api.py
+++ ch02/orders/api/api.py
@@ -5,7 +5,7 @@ from starlette.responses import Response
 from starlette import status
 
 from orders.app import app
-from orders.api.schemas import CreateOrderSchema
+from orders.api.schemas import CreateOrderSchema, GetOrdersSchema, GetOrderSchema
 
 orders = {
     "id": "abcd-1234",
@@ -15,22 +15,22 @@ orders = {
 }
 
 
-@app.get("/orders")
+@app.get("/orders", response_model=GetOrdersSchema)
 def get_orders():
     return {"orders": [orders]}
 
 
-@app.post("/orders", status_code=status.HTTP_201_CREATED)
+@app.post("/orders", status_code=status.HTTP_201_CREATED, response_model=GetOrderSchema)
 def create_order(order_details: CreateOrderSchema):
     return orders
 
 
-@app.get("/orders/{order_id}")
+@app.get("/orders/{order_id}", response_model=GetOrderSchema)
 def get_order(order_id: UUID):
     return orders
 
 
-@app.put("/orders/{order_id}")
+@app.put("/orders/{order_id}", response_model=GetOrderSchema)
 def update_order(order_details: CreateOrderSchema):
     return orders
 
@@ -40,11 +40,11 @@ def delete_order(order_id: UUID):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.post("/orders/{order_id}/cancel")
+@app.post("/orders/{order_id}/cancel", response_model=GetOrderSchema)
 def cancel_order(order_id: UUID):
     return orders
 
 
-@app.post("/orders/{order_id}/pay")
+@app.post("/orders/{order_id}/pay", response_model=GetOrderSchema)
 def pay_order(order_id: UUID):
     return orders
