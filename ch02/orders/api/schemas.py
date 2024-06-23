25f9c8d88b59a9c455a46adc65671d503b242e40
iizk <iizkmaiko@gmail.com>
2024-06-23 11:41:13 +0900

    add: add schemas

---
+++ ch02/orders/api/schemas.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, conint, conlist
+
+
+class Size(Enum):
+    small = "small"
+    medium = "medium"
+    large = "large"
+
+
+class StatusEnum(Enum):
+    created = "created"
+    paid = "paid"
+    progress = "progress"
+    cancelled = "cancelled"
+    dispatched = "dispatched"
+    delivered = "delivered"
+
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: Optional[conint(ge=1, strict=True)] = 1
+
+
+class CreateOrderSchema(BaseModel):
+    order: conlist(OrderItemSchema, min_items=1)
+
+
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: StatusEnum
+
+
+class GetOrderSchema(BaseModel):
+    orders: List[GetOrderSchema]
